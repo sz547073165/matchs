8227c33003eb6fffc16de6591cfc1d9f4b628e85
Marco <sz547073165@qq.com>
2017-10-21 16:59:14 +0800

    循环处理match_detail表的数据

--- match_handler.py
+++ match_handler.py
@@ -9,30 +9,34 @@ import pymysql
 
 conn = pymysql.connect(host='localhost', port=3306, user='spider', passwd='spider', db='matchs', charset='UTF8')
 cursor = conn.cursor()
-
-matchStr = misc.getOneMatchDetail(conn,cursor)
-#matchStr[0]为id，matchStr[1]为simple，matchStr[2]为detail
-
-simpleStr = matchStr[1].replace('["','').replace('"]','')
-simpleList = simpleStr.split('","')
-print(simpleList)
-
-match = []
-match.append(simpleList[0])#id
-match.append(simpleList[2])#league
-match.append(str(simpleList[43])+'-'+str(simpleList[36])+' '+ str(simpleList[11])+':00')#time
-match.append(simpleList[5])#home
-match.append(simpleList[8])#guest
-match.append(simpleList[14])#home_goal
-match.append(simpleList[15])#guest_goal
-match.append(simpleList[16])#home_goal_h
-match.append(simpleList[17])#guest_goal_h
-match.append(simpleList[29])#point
-match.append(simpleList[46])#goals
-match.append(simpleList[48])#home_corner
-match.append(simpleList[49])#guest_corner
-match.append(simpleList[18])#home_red
-match.append(simpleList[19])#guest_red
-match.append(simpleList[20])#home_yellow
-match.append(simpleList[21])#guest_yellow
-print(match)
+matchSelectSql = 'select count(*) from `match` where match_id = %s'
+matchInsertSql = 'insert into `match` (match_id, league, time, home, guest, home_goal, guest_goal, home_goal_h, guest_goal_h, points, goals, home_corner, guest_corner, home_red, guest_red, home_yellow, guest_yellow) '
+matchInsertSql += 'value (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )'
+companyInsertSql = 'insert into company (company_id, company_name, company_country) value (%s, %s, %s)'
+oddsInsertSql = 'insert into odds (match_id, companys_id, time, win, draw, lose, k_win, k_draw, k_lose) value (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+matchDetailDeleteSql = 'delete from match_detail where id = %s'
+isNext = True
+while isNext:
+    matchStr = misc.getOneMatchDetail(conn,cursor)
+    if not matchStr:
+        isNext = False
+        break
+    '''if misc.checkAndDeleteMatchDetail(conn,cursor,matchStr[0]):
+        continue'''
+    print('matchId='+str(matchStr[0]))
+    match = misc.getMatch(matchStr)#获取match
+    companyList, companyMap = misc.getCompanyList(conn, cursor,matchStr)#获取company列表
+    oddsList = misc.getOddsList(matchStr, companyMap)#获取所有odds
+    try:
+        cursor.execute(matchInsertSql, match)
+        for company in companyList:
+            cursor.execute(companyInsertSql, company)
+        for odds in oddsList:
+            cursor.execute(oddsInsertSql, odds)
+        cursor.execute(matchDetailDeleteSql, matchStr[0])
+        conn.commit()
+    except:
+        conn.rollback()
+        print('fail')
+cursor.close
+conn.close
--- misc.py
+++ misc.py
@@ -4,9 +4,92 @@ Created on Fri Oct 20 17:41:27 2017
 
 @author: Marco
 """
+import re
 
 def getOneMatchDetail(conn, cursor):
     sql = 'select id, simple, detail from match_detail limit 0, 1'
     cursor.execute(sql)
-    match = cursor.fetchone()#取一条，fetchall()取所有
+    matchStr = cursor.fetchone()#取一条，fetchall()取所有
+    #matchStr[0]为id，matchStr[1]为simple，matchStr[2]为detail
+    return matchStr
+
+def getMatch(matchStr):
+    simpleStr = matchStr[1].replace('["','').replace('"]','')#获取simple字段部分
+    simpleList = simpleStr.split('","')
+    match = []
+    match.append(int(simpleList[0]))#id
+    match.append(simpleList[2])#league
+    match.append(str(simpleList[43])+'-'+str(simpleList[36])+' '+ str(simpleList[11])+':00')#time
+    match.append(simpleList[5])#home
+    match.append(simpleList[8])#guest
+    match.append(int(simpleList[14]))#home_goal
+    match.append(int(simpleList[15]))#guest_goal
+    match.append(int(simpleList[16]))#home_goal_h
+    match.append(int(simpleList[17]))#guest_goal_h
+    match.append(float(simpleList[29]))#point
+    match.append(float(simpleList[46]))#goals
+    match.append(int(simpleList[48]))#home_corner
+    match.append(int(simpleList[49]))#guest_corner
+    match.append(int(simpleList[18]))#home_red
+    match.append(int(simpleList[19]))#guest_red
+    match.append(int(simpleList[20]))#home_yellow
+    match.append(int(simpleList[21]))#guest_yellow
     return match
+
+def getCompanyList(conn, cursor, matchStr):
+    companyList = []
+    companyMap = {}
+    gamesStr = matchStr[2]#获取detail字段部分
+    gamesList = re.findall(r'var game=Array\(\"(.*?)\"\);',gamesStr)[0].split('","')
+    for game in gamesList:
+        gameList = game.split('|')
+        #print(gameList)
+        companyMap[gameList[1]]=gameList[0]
+        selectSql = 'select company_id from company where company_id = %s'
+        cursor.execute(selectSql,gameList[0])
+        if not cursor.fetchone():
+            company=[]
+            company.append(gameList[0])#company_id
+            company.append(gameList[2])#company_name
+            if re.findall(r'\((.*?)\)',gameList[21]):
+                company.append(re.findall(r'\((.*?)\)',gameList[21])[0])#company_country
+            else:
+                company.append('')
+            companyList.append(company)
+    return companyList, companyMap
+
+def getOddsList(matchStr, companyMap):
+    matchId = matchStr[1].replace('["','').replace('"]','').split('","')[0]
+    year = matchStr[1].replace('["','').replace('"]','').split('","')[43]
+    gameDetailsStr = matchStr[2]#获取detail字段部分
+    gameDetailsList = re.findall(r'gameDetail=Array\(\"(.*?)\"\);',gameDetailsStr)[0].split('","')
+    oddsList = []
+    for gameDetails in gameDetailsList:
+        subCompanyId = re.findall(r'(.*?)\^',gameDetails)[0]
+        gameDetailList = gameDetails.split('^')[1].split(';')
+        for gameDetail in gameDetailList:
+            if gameDetail == '':
+                continue
+            tempList = gameDetail.split('|')
+            odds = []
+            odds.append(matchId)
+            odds.append(companyMap[subCompanyId])
+            odds.append(year+'-'+tempList[3]+':00')
+            odds.append(tempList[0])
+            odds.append(tempList[1])
+            odds.append(tempList[2])
+            odds.append(tempList[4])
+            odds.append(tempList[5])
+            odds.append(tempList[6])
+            oddsList.append(odds)
+    return oddsList
+
+def checkAndDeleteMatchDetail(conn, cursor, matchId):
+    matchSelectSql = 'select match_id from `match` where match_id = %s'
+    cursor.execute(matchSelectSql, matchId)
+    if cursor.fetchone():
+        matchDetailDeleteSql = 'delete from match_detail where id = %s'
+        cursor.execute(matchDetailDeleteSql, matchId)
+        conn.commit()
+        return True
+    return False
